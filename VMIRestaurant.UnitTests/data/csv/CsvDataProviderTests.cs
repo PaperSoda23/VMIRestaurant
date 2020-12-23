e1331c132ef7b98eeeb3dd7b0def740fbff2efa9
Jack <eljukna@gmail.com>
2020-12-23 23:45:36 +0200

    add data reading from csv tests

---
+++ VMIRestaurant.UnitTests/data/csv/CsvDataProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Moq;
+using VMIRestaurant.common;
+using VMIRestaurant.data.csv;
+using VMIRestaurant.data.csv.mappers;
+using Xunit;
+
+namespace VMIRestaurant.UnitTests.data.csv
+{
+    public class CsvDataProviderTests
+    {
+        private class MockEntity : IEntity { public int Id { get; set; } }
+
+        [Fact]
+        public void provides_data()
+        {
+            var csvData = new List<List<string>> {new List<string> {"1"}, new List<string> {"2"}};
+
+            var mockMapper = new Mock<ICsvEntityMapper>();
+            mockMapper.Setup(m => m.MapToEntity(csvData[0])).Returns(new MockEntity {Id = 1});
+            mockMapper.Setup(m => m.MapToEntity(csvData[1])).Returns(new MockEntity {Id = 2});
+
+            var result = CsvDataProvider<MockEntity>.ProvideData(csvData, mockMapper.Object);
+            
+            Assert.Equal(result.Count, csvData.Count);
+            Assert.IsType<MockEntity>(result[0]);
+            Assert.Equal(1, result[0].Id);
+            Assert.Equal(2, result[1].Id);
+        }
+    }
+}
---
+++ VMIRestaurant.UnitTests/data/csv/CsvFileReaderTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using VMIRestaurant.data.csv;
+using Xunit;
+
+namespace VMIRestaurant.UnitTests.data.csv
+{
+    public class CsvFileReaderTests
+    {
+        private static readonly string TestFileDirectory = GetTestDataDirectory();
+        
+        private static string GetTestDataDirectory()
+        {
+            var rootTestProjectDirectory = Path.GetDirectoryName(Assembly
+                .GetExecutingAssembly().Location.Replace(@"bin\Debug\netcoreapp3.1", string.Empty));
+            
+            var dataDirectory = Path.Join(rootTestProjectDirectory, @"\data\csv\data");
+
+            return dataDirectory;
+        }
+        
+        [Fact]
+        public async Task errors_when_file_is_not_found()
+        {
+            var ex = await Assert.ThrowsAsync<ArgumentException>(
+                () => CsvFileReader.ReadData(TestFileDirectory, "random.csv")
+            );
+            
+            Assert.Contains("file random.csv does not exist in directory", ex.Message);
+        }
+        
+        [Fact]
+        public async Task errors_when_file_is_not_of_type_csv()
+        {
+            var ex = await Assert.ThrowsAsync<ArgumentException>(
+                () => CsvFileReader.ReadData(TestFileDirectory, "file.txt")
+            );
+            Assert.Equal("file file.txt is not a csv file", ex.Message);
+        }
+
+        [Fact]
+        public async Task reads_csv_file()
+        {
+            var data = await CsvFileReader.ReadData(TestFileDirectory, "file.csv");
+            
+            Assert.Equal(2, data.Count);
+            Assert.Equal(data[0], new List<string>{"hello", "world"});
+            Assert.Equal(data[1], new List<string>{"bye", "world"});
+        }
+    }
+}
