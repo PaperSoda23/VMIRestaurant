50a8dfae0f11057af2f752f207525a1f7b6da4a1
Jack <eljukna@gmail.com>
2020-12-24 22:43:23 +0200

    add dish-repo, ingredient-repo tests

---
+++ VMIRestaurant.UnitTests/data/repository/DishRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using VMIRestaurant.data.repository;
+using VMIRestaurant.domain.restaurant;
+using Xunit;
+
+namespace VMIRestaurant.UnitTests.data.repository
+{
+    public class DishRepositoryTests
+    {
+        [Fact]
+        public void checks_by_name_if_entity_exists()
+        {
+            var dishes = new List<Dish> {new Dish {Id=0, Name = "One"}, new Dish {Id=1, Name = "Two"}};
+            
+            var dishRepository = new DishRepository(dishes);
+            
+            Assert.True(dishRepository.ExistsByName("One"));
+            Assert.False(dishRepository.ExistsByName("Three"));
+        }
+    }
+}
---
+++ VMIRestaurant.UnitTests/data/repository/IngredientRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using VMIRestaurant.data.repository;
+using VMIRestaurant.domain.restaurant;
+using Xunit;
+
+namespace VMIRestaurant.UnitTests.data.repository
+{
+    public class IngredientRepositoryTests
+    {
+        [Fact]
+        public void checks_by_name_if_ingredient_exists()
+        {
+            var ingredients = new List<Ingredient> {new Ingredient {Id=0, Name = "One"}};
+            
+            var ingredientRepository = new IngredientRepository(ingredients);
+            
+            Assert.True(ingredientRepository.ExistsByName("One"));
+            Assert.False(ingredientRepository.ExistsByName("Three"));
+        }
+
+        [Fact]
+        public void gets_ingredient_by_name()
+        {
+            var ingredients = new List<Ingredient> {new Ingredient {Id=0, Name = "ExistingName"}};
+            
+            var ingredientRepository = new IngredientRepository(ingredients);
+            
+            Assert.Equal(ingredientRepository.FindByName("ExistingName"), ingredients[0]);
+            Assert.Null(ingredientRepository.FindByName("NotExitingName"));
+        }
+    }
+}
